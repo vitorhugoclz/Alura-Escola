a68de18630baaf56046f7bdb753ce03f0ff8297b
vitorhugoclz <vitorhugoclz@gmail.com>
2021-04-19 16:31:39 -0300

    aula 02-Model, Admin e Serialiazer

--- escola_api/escola/admin.py
+++ escola_api/escola/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
 # Register your models here.
+
+class Alunos(admin.ModelAdmin):
+  list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+  list_display_link = ('id', 'nome')
+  search_fields = ('nome',)
+  list_per_page = 20
+
+class Cursos(admin.ModelAdmin):
+  list_display = ('id', 'codigo_curso', 'descricao')
+  list_display_link = ('id', 'codigo_curso')
+  search_fields = ('codigo_curso',)
+  list_per_page = 20
+
+admin.site.register(Aluno, Alunos)
+admin.site.register(Curso, Cursos)
