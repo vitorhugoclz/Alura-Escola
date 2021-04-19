725edb5484bfc960116269fff54e68f7b8f24e0f
vitorhugoclz <vitorhugoclz@gmail.com>
2021-04-19 16:04:40 -0300

    aula 01-Instalando Django Rest CONCLUIDA

--- escola_api/escola/views.py
+++ escola_api/escola/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def alunos(request):
+  if request.method == 'GET':
+    aluno = {'id': 1, 'nome': 'Guilherme'}
+    return JsonResponse(aluno)
