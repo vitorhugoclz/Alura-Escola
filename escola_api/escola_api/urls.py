cbc749bdfa9b4f066c3a6fae522f8b794a5c55dc
vitorhugoclz <vitorhugoclz@gmail.com>
2021-04-19 17:02:53 -0300

    aula 03- Viewset, Urls e requisicoes GET e POST

--- escola_api/escola_api/urls.py
+++ escola_api/escola_api/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from escola.views import alunos
+from django.urls import path, include
+from escola.views import AlunosViewSet, CursosViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos)
+    path('', include(router.urls))
 ]
